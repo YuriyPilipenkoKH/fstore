c58e4f4355545e008c48789d80307e2426f98f51
Yuriy Pilipenko <yurik2061@gmail.com>
2025-03-17 10:53:09 +0200

    express api

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello, Node.js!");
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import { corsOptions } from './lib/cors.js';
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 5500;
+
+app.use(cors(corsOptions))
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`)
+
+} );
