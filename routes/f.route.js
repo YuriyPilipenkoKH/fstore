3734bab08615ccce8f7e40f47437dbbc182a4e13
Yuriy Pilipenko <yurik2061@gmail.com>
2025-03-17 11:05:24 +0200

    return array

--- routes/f.route.js
+++ routes/f.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import { fiveLetterWords } from '../data/words.js';
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "F route is working!" });
+  res.json({
+    message: "F route is working!" ,
+    words: fiveLetterWords
+    });
 });
 
 export default router;
