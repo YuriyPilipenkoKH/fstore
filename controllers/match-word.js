cc60fbb1c38aa294880a1ec4c0b99f376030740b
Yuriy Pilipenko <yurik2061@gmail.com>
2025-03-20 16:46:14 +0200

    corrected status code

--- controllers/match-word.js
+++ controllers/match-word.js
@@ -4,6 +4,9 @@ export const matchWord = async (req, res) => {
   const {word} = req.body
   if (!word) {
     console.log('word is required');
+    res.status(400).json({
+      message: "word is required",
+    });
    }
   try {
     if(programmingWords.includes(word)){ 
@@ -13,7 +16,7 @@ export const matchWord = async (req, res) => {
       });
     }
     else {
-      res.status(400).json({
+      res.status(200).json({
         message: "Wrong",
         isMatched: false ,
       });
